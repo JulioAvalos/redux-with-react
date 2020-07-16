5ab4998d9176c702f91e0e3827c2013a8d11df26
JulioAvalos <julioavalos2@msn.com>
2020-07-15 19:36:21 -0600

    combining multiple reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
-    counter: 0,
-    results: []
+    counter: 0
 };
 
 const reducer = (state = initialState, action) => {
@@ -26,19 +25,6 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 counter: state.counter - action.val
-            }
-        case actionTypes.STORE_RESULT: 
-            return {
-                ...state,
-                // result: state.results.push() // dont: still keeps the reference of the original object!
-                results: state.results.concat({id: new Date(), value: state.counter})
-            }
-        case actionTypes.DELETE_RESULT: 
-            // filter returns a new array!
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
             }
     }
     return state;
