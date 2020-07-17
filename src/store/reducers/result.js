3cbf4d0cbc1602f0724b197798ad5dae57b3fac0
JulioAvalos <julioavalos2@msn.com>
2020-07-16 22:45:18 -0600

    a leaner switch case statement

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,15 +5,15 @@ const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updatedObject(state, {results: updatedArray});
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.STORE_RESULT: 
-            // reducers are the best to handle extra logic
-            return updatedObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})});
-        case actionTypes.DELETE_RESULT: 
-            // filter returns a new array!
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updatedObject(state, {results: updatedArray});
+        case actionTypes.STORE_RESULT : return updatedObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})});
+        case actionTypes.DELETE_RESULT : return deleteResult(state, action);
     }
     return state;
 };
